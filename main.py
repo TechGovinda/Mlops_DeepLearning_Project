fe00932d9d8bc4128722ace6a6fdb61c93a51719
govinda sah <sahgovinda401@gmail.com>
2025-04-29 02:03:02 +0530

    3rd commit

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
 
 stage_Name = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>> stage{stage_Name} started<<<<<<<<<<<<<<<<<<<<<")
+    logger.info(f">>>>>> stage {stage_Name} started<<<<<<<<<<<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+
     data_ingestion.main()
     logger.info(f">>>>>>> stage {stage_Name} completed<<<<<<<<<\n x=================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+stage_Name = "Prepare base model"
+try:
+    logger.info(f">>>>>> stage {stage_Name} started<<<<<<<<<<<<<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+
+    prepare_base_model.main()
+    logger.info(f">>>>>>> stage {stage_Name} completed<<<<<<<<<\n x=================x")
+
 except Exception as e:
     logger.exception(e)
     raise e
